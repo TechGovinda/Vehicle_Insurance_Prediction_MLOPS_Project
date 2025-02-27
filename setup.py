8289ca311b10f3b0d7961968628855d654584cf6
govinda sah <sahgovinda401@gmail.com>
2025-02-27 09:33:03 +0530

    data ingestion completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Govinda Sah",
+    author_email="sahgovinda401@gmail.com",
+    packages=find_packages(),
+)
